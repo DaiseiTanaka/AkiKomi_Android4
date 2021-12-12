adad037e09d809a18ca7349d8f03477b2a84f11e
Daisei Tanaka <d.tnk.4004@gmail.com>
2021-12-12 14:35:29 +0900

    first commit

--- app/src/main/java/com/example/planeto/MainActivity.kt
+++ app/src/main/java/com/example/planeto/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 //
 //            // Set Progress Max
 //            progressMax = 100f
+        
 //
 //            // Other
 //            roundBorder = true
