278d4b5b150b0f1b878dbd53e457ed1249e50d44
Daisei Tanaka <d.tnk.4004@gmail.com>
2021-12-17 15:53:33 +0900

    mqtt

--- app/src/main/java/com/example/planeto/MainActivity.kt
+++ app/src/main/java/com/example/planeto/MainActivity.kt
@@ -3,12 +3,53 @@ package com.example.planeto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_planet_detail.*
 import kotlinx.android.synthetic.main.view.*
+import com.example.anoopm.mqtt.manager.MQTTConnectionParams
+import com.example.anoopm.mqtt.manager.MQTTmanager
+import com.example.anoopm.mqtt.protocols.UIUpdaterInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , UIUpdaterInterface {
+
+    var mqttManager:MQTTmanager? = null
+
+
+
+
+    // Interface methods
+    override fun resetUIWithConnection(status: Boolean) {
+
+//        ipAddressField.isEnabled  = !status
+//        topicField.isEnabled      = !status
+//        messageField.isEnabled    = status
+ //         connectBtn.isEnabled      = !status
+//        sendBtn.isEnabled         = status
+
+    // Update the status label.
+//        if (status){
+//            updateStatusViewWith("Connected")
+//        }else{
+//            updateStatusViewWith("Disconnected")
+//        }
+    }
+
+    override fun updateStatusViewWith(status: String) {
+        //statusLabl.text = status
+    }
+
+    override fun update(message: String) {
+
+        var text = messageHistoryView.text.toString()
+        var newText = "$message"
+        //var newText = text.toString() + "\n" + message +  "\n"
+        messageHistoryView.setText(newText)
+        //messageHistoryView.setSelection(messageHistoryView.text.length)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +58,21 @@ class MainActivity : AppCompatActivity() {
         my_recycler.adapter=Adapter(setData.SetPlanets())
         my_recycler.layoutManager=LinearLayoutManager(this)
 
+        resetUIWithConnection(false)
+
+//        circularProgressBar.apply {
+//            //setProgressWithAnimation(50f,1000)
+//            progress = 10f
+//        }
     }
+
+    fun connect(view: View){
+            var host = "tcp://" + "192.168.1.111" + ":1883"
+            var topic = "topic_advanced"
+            var connectionParams = MQTTConnectionParams("MQTTSample",host,topic,"","")
+            mqttManager = MQTTmanager(connectionParams,applicationContext,this)
+            mqttManager?.connect()
+
+    }
+
 }
--- app/src/main/java/com/example/planeto/setData.kt
+++ app/src/main/java/com/example/planeto/setData.kt
@@ -10,7 +10,7 @@ object setData {
                 "South Building",
                 "4th",
                 "70",
-                "This is the library on the 4th floor of the South Building. The library is divided into two levels. There are two projectors on the first floor and various types of tables and chairs. On the first floor you are allowed to work on tasks while talking to your friends. On the 2nd floor, there are many engineering books and you can borrow them freely if you have a student ID card. However, talking is prohibited on the 2nd floor."
+                "This is the library on the 4th floor of the South Building. The library is divided into two levels. There are two projectors on the first floor and various types of tables and chairs. On the first floor you are allowed to work on tasks while talking to your friends. On the 2nd floor, there are many engineering books and you can borrow them freely if you have a student ID card. However, talking is prohibited on the 2nd floor.",
                 "topic_slibrary"
             )
         )
@@ -21,7 +21,7 @@ object setData {
                 "South Building",
                 "2nd",
                 "108",
-                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study."
+                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study.",
                 "topic_lc1"
             )
         )
@@ -32,7 +32,7 @@ object setData {
                 "South Building",
                 "2nd",
                 "82",
-                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study."
+                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study.",
                 "topic_lc2"
             )
         )
@@ -43,7 +43,7 @@ object setData {
                 "South Building",
                 "3rd",
                 "59",
-                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study."
+                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study.",
                 "topic_lc3"
             )
         )
@@ -54,7 +54,7 @@ object setData {
                 "South Building",
                 "4th",
                 "116",
-                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study."
+                "There are in total of four Learning Commons in this South Building. There are desks and chairs that can be used freely by students. This is the place that is designed for interaction with students from other grades and also graduate students. You can also use the air conditioner and whiteboard freely for meetings and self-study.",
                 "topic_lc4"
             )
         )
@@ -65,7 +65,7 @@ object setData {
                 "South Building",
                 "3rd",
                 "216",
-                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature."
+                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature.",
                 "topic_s306"
             )
         )
@@ -76,7 +76,7 @@ object setData {
                 "South Building",
                 "3rd",
                 "216",
-                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature."
+                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature.",
                 "topic_s307"
             )
         )
@@ -87,7 +87,7 @@ object setData {
                 "South Building",
                 "3rd",
                 "108",
-                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature."
+                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature.",
                 "topic_s311"
             )
         )
@@ -98,7 +98,7 @@ object setData {
                 "South Building",
                 "3rd",
                 "108",
-                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature."
+                "This room is usually used for students in the Faculty of Engineering. There are 3 screens so you can take the same lesson simultaneoulsy as the next room. After school, students are free to remain and study by themselves. The seats are not fixed and each student sits freely every morning. You are allowed to use airconditioner and adjust the temperature.",
                 "topic_s312"
             )
         )
@@ -109,7 +109,7 @@ object setData {
                 "South Building",
                 "1st",
                 "168",
-                "This room can be freely used by students and faculty members. Many people have lunch during lunch break and use this room as a study room after school. There are many desks and chairs that can be used freely. At the beginning of each semester, this room is used as medical examinations and selling textbooks. In addition, there are two large windows, from which you can overlook the garden, creating a space with a sense of openness."
+                "This room can be freely used by students and faculty members. Many people have lunch during lunch break and use this room as a study room after school. There are many desks and chairs that can be used freely. At the beginning of each semester, this room is used as medical examinations and selling textbooks. In addition, there are two large windows, from which you can overlook the garden, creating a space with a sense of openness.",
                 "topic_advanced"
             )
         )
@@ -120,7 +120,7 @@ object setData {
                 "South Building",
                 "1st",
                 "360",
-                "This is a hall where meetings such as events and orientations are held. Since one of the wall is made of window glass, you can overlook the courtyard and feel a sense of openness. Additionaly, this hall is the largest hall in the South Building and can accommodate up to 360 people. Also, if the chairs and desks are removed, more than 500 people can enter the room."
+                "This is a hall where meetings such as events and orientations are held. Since one of the wall is made of window glass, you can overlook the courtyard and feel a sense of openness. Additionaly, this hall is the largest hall in the South Building and can accommodate up to 360 people. Also, if the chairs and desks are removed, more than 500 people can enter the room.",
                 "topic_sagano"
             )
         )
@@ -131,7 +131,7 @@ object setData {
                 "South Building",
                 "1st",
                 "40",
-                "Many of the latest machines and 3D printers are installed in this machine workshop. Some experiments and classes dealing with machines are conducted in this room. This workshop holds an annual 3D printer competition. In addition, the 3D printer in this workshop can be used by anyone who has permission to it. Also it is used for many cornerstone projects. To use this workshop, you need to attend the workshop orientation. If you are curious, please contact the student center or the teacher at the workshop."
+                "Many of the latest machines and 3D printers are installed in this machine workshop. Some experiments and classes dealing with machines are conducted in this room. This workshop holds an annual 3D printer competition. In addition, the 3D printer in this workshop can be used by anyone who has permission to it. Also it is used for many cornerstone projects. To use this workshop, you need to attend the workshop orientation. If you are curious, please contact the student center or the teacher at the workshop.",
                 "topic_mws"
             )
         )
@@ -142,7 +142,7 @@ object setData {
                 "South Building",
                 "4th",
                 "25",
-                "This electronic workshop is equipped with many of the latest equipment. It can be used to conducts experiments and classes related to electricity. In addition, the walls on the south and north sides are all covered with glass, creating a very open space. In addition, you can go directly to Learning Commons from this electronic workshop where you can have meals and meetings. It is shared by several Cornerstone projects and used by many project members after school as well as holidays."
+                "This electronic workshop is equipped with many of the latest equipment. It can be used to conducts experiments and classes related to electricity. In addition, the walls on the south and north sides are all covered with glass, creating a very open space. In addition, you can go directly to Learning Commons from this electronic workshop where you can have meals and meetings. It is shared by several Cornerstone projects and used by many project members after school as well as holidays.",
                 "topic_ews"
             )
         )
